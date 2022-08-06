ec500c154d7e519d03e8c1ddd2295831988ec534
AsArunas <arunas.saltis.as@gmail.com>
2022-08-06 19:06:50 +0300

    first commit

---
+++ Index.js
@@ -0,0 +1,84 @@
+let arr = [
+  {
+    id: 123,
+    task: "buy milk",
+  },
+  {
+    id: 321,
+    task: "buy eggs",
+  },
+  {
+    id: 234,
+    task: "clean my room",
+  },
+  {
+    id: 543,
+    task: "watch any js video in youtube",
+  },
+];
+
+document.getElementById("addBta").addEventListener("click", () => {
+  const inputValue = document.getElementById("taskInput").value;
+  const input = document.getElementById("taskInput");
+
+  if (!inputValue.trim()) {
+    alert("Incorrect value. Check if provided");
+    input.value = null;
+    return;
+  }
+
+  const newTask = {
+    id: new Date().getTime(),
+    task: inputValue,
+  };
+
+  arr.push(newTask);
+  input.value = null;
+  drawTaskList();
+});
+
+function deleteItem() {
+  console.log(this.id);
+  arr = arr.filter((val) => val.id !== +this.id);
+  drawTaskList();
+}
+
+const drawTaskList = () => {
+  const tasksList = document.getElementById("tasksList");
+  tasksList.innerHTML = null;
+
+  arr.forEach((value, ind) => {
+    //creating elements
+    const myLi = document.createElement("li");
+    const myInput = document.createElement("input");
+    const myLabel = document.createElement("label");
+    const deleteBtn = document.createElement("button");
+
+    //adding styles
+    myLi.className = "list-group-item";
+    myInput.className = "form-check-input me-1";
+    myLabel.className = "form-check-label";
+    deleteBtn.className = "btn btn-danger btn-sm col-2";
+
+    //adding other atributes
+    //input
+    myInput.setAttribute("type", "checkbox");
+    myInput.setAttribute("id", ind);
+
+    //label
+    myLabel.setAttribute("for", ind);
+    myLabel.textContent = value.task;
+
+    //delete btn
+    deleteBtn.setAttribute("type", "button");
+    deleteBtn.setAttribute("id", value.id);
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = deleteItem;
+
+    //append childs
+    myLi.append(myInput, myLabel, deleteBtn);
+    tasksList.append(myLi);
+  });
+};
+
+drawTaskList();
